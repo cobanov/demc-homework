47e70a8aed6b83b3a2e027fedf1cd74c397aa8ae
Mert Cobanov <mertcobanov@gmail.com>
2021-03-07 00:15:45 +0300

    update

--- app/main.py
+++ app/main.py
@@ -20,10 +20,10 @@ def rick():
     conn = db_utils.connect_db()
     top_avg = db_utils.read_data(conn, queries.top_avg)
     worst_avg = db_utils.read_data(conn, queries.worst_avg)
-    # top_vote = db_utils.read_data(conn, queries.top_vote)
-    # worst_vote = db_utils.read_data(conn, queries.worst_vote)
-    # results = [top_avg, worst_avg, top_vote, worst_vote]
-    results = [top_avg, worst_avg]
+    top_vote = db_utils.read_data(conn, queries.top_vote)
+    worst_vote = db_utils.read_data(conn, queries.worst_vote)
+    results = [top_avg, worst_avg, top_vote, worst_vote]
+    # results = [top_avg, worst_avg]
 
     print(results)
     return render_template("rick.html", results=results)
@@ -31,6 +31,8 @@ def rick():
 
 @app.route("/general", methods=["GET", "POST"])
 def general():
-    """ some codes """
+    conn = db_utils.connect_db()
+    seven_b = db_utils.read_data(conn, queries.seven_b)
+    seven_c = db_utils.read_data(conn, queries.seven_c)
 
-    return render_template("general.html")
+    return render_template("general.html", seven_b=seven_b, seven_c=seven_c)
--- app/queries.py
+++ app/queries.py
@@ -1,6 +1,6 @@
 top_avg = """SELECT *
 FROM rm_episodes
-LEFT JOIN ratings
+INNER JOIN ratings
 ON rm_episodes.tconst = ratings.tconst
 ORDER BY averageRating
 DESC
@@ -8,7 +8,7 @@ LIMIT 5
 """
 worst_avg = """SELECT *
 FROM rm_episodes
-LEFT JOIN ratings
+INNER JOIN ratings
 ON rm_episodes.tconst = ratings.tconst
 ORDER BY averageRating
 ASC
@@ -17,7 +17,7 @@ LIMIT 5
 
 top_vote = """SELECT *
 FROM rm_episodes
-LEFT JOIN ratings
+INNER JOIN ratings
 ON rm_episodes.tconst = ratings.tconst
 ORDER BY numVotes
 DESC
@@ -26,9 +26,19 @@ LIMIT 5
 
 worst_vote = """SELECT *
 FROM rm_episodes
-LEFT JOIN ratings
+INNER JOIN ratings
 ON rm_episodes.tconst = ratings.tconst
 ORDER BY numVotes
 ASC
 LIMIT 5
 """
+
+seven_b = """SELECT * 
+FROM name_basics 
+WHERE birthYear > 2000 
+AND primaryProfession 
+LIKE '%music_department%' 
+AND primaryProfession 
+LIKE '%actress%' """
+
+seven_c = """select count(*) from four_actress """
